d6e16b5a0717b1230bf0e086a4aa92e7e98fe967
jekaH1 <ceyhun100203@gmail.com>
2023-08-31 17:28:44 +0400

    input groups added

--- ProniaBackEnd/Controllers/ProductController.cs
+++ ProniaBackEnd/Controllers/ProductController.cs
@@ -39,34 +39,6 @@ namespace ProniaBackEnd.Controllers
             return View(productDetailViewModel);
         }
 
-        //[HttpPost]
-        //public IActionResult AddToBasket(int id)
-        //{
-        //    Product product = _appDbContext.Products.FirstOrDefault(x => x.Id == id);
-        //    if (product is null) { return View(NotFoundConstants.NotFoundProniaUrl); }
-
-        //    BasketItem basketItem = _appDbContext.BasketItems.FirstOrDefault(x => x.ProductId == product.Id);
-        //    if (basketItem is null)
-        //    {
-        //        basketItem = new BasketItem();
-        //        basketItem.ProductId = product.Id;
-        //        basketItem.Color =
-
-        //        _appDbContext.BasketItems.Add(basketItem);
-
-        //    }
-        //    else
-        //    {
-        //        basketItem.Quantity += 1;
-        //        basketItem.Product.ProductCategories = _appDbContext.ProductCategory.Where(x => x.ProductId == product.Id).ToList();
-        //        basketItem.Product.ProductSizes = _appDbContext.ProductSize.Where(x => x.ProductId == product.Id).ToList();
-        //        basketItem.Product.ProductColors = _appDbContext.ProductColor.Where(x => x.ProductId == product.Id).ToList();
-        //        basketItem.TotalPrice = product.Price * basketItem.Quantity;
-        //    }
-        //    _appDbContext.SaveChanges();
-        //    return Ok(basketItem);
-        //}
-
         [HttpPost]
         public IActionResult AddToBasket(ProductDetailViewModel viewModel, int? id)
         {
